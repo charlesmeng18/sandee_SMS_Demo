4d69cd100fa6a5ed2bfea075aae4999791e78ea3
charlesmeng18 <cmeng@g.hmc.edu>
2020-06-12 12:03:29 -0700

    cleaned up App.js file

--- src/App.js
+++ src/App.js
@@ -3,33 +3,10 @@ import './App.css';
 import SMSForm from './SMS/SMSForm'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      greeting: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
           <SMSForm />
         </header>
       </div>
